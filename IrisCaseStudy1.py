182038fde591fdf20836282648da596c2b56bceb
Ankita Somwanshi <99323355+AnkitaSomwanshi@users.noreply.github.com>
2026-08-31 00:18:58 +0530

    Add files via upload

---
+++ IrisCaseStudy1.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_iris
+
+
+def main():
+
+    print("-"*30)
+    print("IrisClassification Case Study")
+    print("-"*30)
+
+    Dataset = load_iris()
+
+    print(Dataset)
+
+if __name__ == "__main__":
+    main()
---
+++ IrisCaseStudy3.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_iris
+
+
+def main():
+
+    print("-"*30)
+    print("IrisClassification Case Study")
+    print("-"*30)
+
+    Dataset = load_iris()
+
+    # Metadata of the dataset
+
+    print("Independent Variables are : ")
+    print(Dataset.feature_names)
+    print("Lenght of Independent Variables : ",len(Dataset.feature_names))
+
+    print("Dependent Variables are : ")
+    print(Dataset.target_names)
+    print("Lenght of Dependent Variables : ",len(Dataset.target_names))
+
+
+if __name__ == "__main__":
+    main()
