b050e55f58e576f75c0226ba67b457e6d3a661f4
Ankita Somwanshi <99323355+AnkitaSomwanshi@users.noreply.github.com>
2026-09-02 22:03:39 +0530

    Add files via upload

---
+++ MatplotlibBarPlot.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+def main():
+
+    language = ["C","C++","Java","Python"]
+    students = [30,40,35,55]
+
+    plt.bar(
+        language,
+        students,
+        width =0.6,             # Width of Bars
+        edgecolor = "Black",    # Border color of bars
+        linewidth = 1,          # width of bar border
+        alpha = 0.8,            # Transperance 0.0 to 1.0
+        label = "Students"      # legend text
+    ) 
+
+    plt.title("Marvellous Bar Plot")
+    plt.xlabel("Languages")
+    plt.ylabel("Number of students")
+    
+    #plt.grid(True)
+    
+    plt.legend()
+    
+    plt.show()
+
+if __name__ == "__main__":
+    main()
